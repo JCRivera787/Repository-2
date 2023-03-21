f103e266c859d7c81a312a36a9932393a830125c
JCRivera787 <jeancapsp@yahoo.com>
2023-03-20 19:54:12 -0500

    Grocery List

---
+++ Coding/Script.js
@@ -0,0 +1,27 @@
+//Brushing your teeth//
+Step on how to bruch your teeth
+
+Grab a toothbrush with the right  hand.
+Turn on the water with the left hand.
+Rinse the brush part of the toothbrush with water.
+Turn off the water with the left hand.
+Open the tube of toothpaste utilizing your fingers.
+Squeeze toothpaste  gently on the brush part of the toothbrush.
+Start with the front teeth all the way to the back.
+Move in circular motions with the brush.
+
+const greeting= "Hello World";
+
+console.log("Hello World");
+
+alert("Hello World");
+
+const numUmbrellas = 10 * 3;
+
+const num = 5 + 1;
+alert(num);
+const str = 'Hello' + ' ' + 'World';
+let str1 = 'Hello,';
+let str2 = 'I am';
+let name = 'Josh'; 
+alert(str1 + " " + str2 + " " + name);
